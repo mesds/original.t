cd474386fe24986494fe64165a46ef569b26bf6a
Mari <85968726+mesds@users.noreply.github.com>
2022-05-09 12:21:15 -0300

    Create ap.js

---
+++ ap.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import GlobalStyles from "./styles/global";
+import Product from "./pages/Product";
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Product />
+    </>
+  );
+}
